2d09ee78f2a1032809ee0ec5fa590152b1b75175
Animesh Khanra <animeshkhanra2022@gmail.com>
2025-10-21 14:03:35 +0530

    This is the first commit and clean up app.jsx, install some packages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl bg-amber-100 inline-block'>hi i am animesh</h1>
+    </>
+  )
+}
+
+export default App
