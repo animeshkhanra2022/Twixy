11422b9ca6c1c839475e2a60770e0b27a77baae6
Animesh Khanra <animeshkhanra2022@gmail.com>
2025-10-24 16:41:34 +0530

    Implement Appwrite authentication and integrate Redux for user state management; update dependencies and add Header/Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components/index'
+import { Outlet } from 'react-router-dom'
+
 
 
 function App() {
-  const name = console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1 className='text-6xl bg-amber-100 inline-block'>hi i am animesh</h1>
-    </>
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+      
+    </div>
   )
+    : null
 }
 
 export default App
